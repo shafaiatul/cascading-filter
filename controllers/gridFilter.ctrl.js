4c54cfc28f852a56eecd20cd24eef78fc49a11d7
Shafaiatul Islam <shafaiatul@gmail.com>
2017-08-18 15:56:28 -0400

    Update gridFilter.ctrl.js

--- controllers/gridFilter.ctrl.js
+++ controllers/gridFilter.ctrl.js
@@ -2,24 +2,41 @@
     'use strict';
     app
     .controller('gridFilter', function ($scope, allData) {
-        $scope.itemSelecteds = {};
-        $scope.dummyModel = {};
-        allData.getGridData().then((response) => {
-            $scope.items = response.data;
-        })
-        
-        
-        $scope.selectItem = function (item) {
-          // If checkbox is checked
-          if ($scope.dummyModel[item.id]) {
-            $scope.itemSelecteds[item.id] = item;
-          } 
-        }
-        
-        $scope.update = function () {
-          console.log($scope.itemSelecteds);
-        }
-        
+      allData.getGridData().then( (response) => {
+        $scope.providers = response.data;
+      })
+      
+      $scope.user = {
+        providers: []
+      };
+      $scope.checkAll = function() {
+        $scope.user.providers = angular.copy($scope.providers);
+      };
+      $scope.uncheckAll = function() {
+        $scope.user.providers = [];
+      };
+      // Report Type
+      $scope.selectedReportType = "";
+      $scope.user = {
+          reportTypes: []
+      };
+      
+      $scope.checkAllFilterTypes = function(d) {
+        console.log(d);
+          $scope.user.reportTypes = d.map(function(item) { return item.reportId; });
+      };
+      $scope.uncheckAllFilterTypes = function() {
+          $scope.user.reportTypes = [];
+      };
+
+
+
+
+
+
+      $scope.Submit= function() {
+        console.log($scope.user.providers);
+      }
         
     })
 
